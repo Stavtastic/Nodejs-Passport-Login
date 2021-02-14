a125747e7f69f9783f9173061c2d4baf34d80733
Stavtastic <justin.stavasius@gmail.com>
2021-02-14 08:30:14 +0100

    update
    update

--- server.js
+++ server.js
@@ -2,52 +2,61 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
-const initializePassport = require('./passport-config')
+const initializePassport = require('./passport-config');
 initializePassport(
   passport,
   email => users.find(user => user.email === email),
   id => users.find(user => user.id === id)
-)
+);
 
-const users = []
+// LowDB (because I suck with database technology
+const low = require('lowdb')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+const FileSync = require('lowdb/adapters/FileSync')
+// Set some defaults (required if your JSON file is empty)
+db.defaults({users: []})
+    .write();
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
+//const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
 app.get('/', checkAuthenticated, (req, res) => {
   res.render('index.ejs', { name: req.user.name })
-})
+});
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render('login.ejs')
-})
+});
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
-}))
+}));
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
   res.render('register.ejs')
-})
+});
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
@@ -57,17 +66,17 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
-    })
+    });
     res.redirect('/login')
   } catch {
     res.redirect('/register')
   }
-})
+});
 
 app.delete('/logout', (req, res) => {
   req.logOut()
   res.redirect('/login')
-})
+});
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -84,4 +93,4 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000)
+app.listen(3000);
